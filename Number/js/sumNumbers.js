b0495e5148d9ec8e0d68aa623ddfaea98f5b708d
Polina Vasilevich <polina.vasilevichh@gmail.com>
2020-11-13 14:43:50 +0300

    added files on the topic NUMBERS

---
+++ Number/js/sumNumbers.js
@@ -0,0 +1,9 @@
+// Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt) и после показывает их сумму.
+
+let firstNumber = +prompt('Enter a number:');
+let secondNumber = +prompt('Enter a number:');
+
+console.log(firstNumber + secondNumber);
+
+
+
